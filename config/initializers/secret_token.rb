959652c7b5ee8a062bf7a9f50a8aa00e205ba952
pacing <pacing@pacing-Aspire-4741.(none)>
2013-03-17 02:22:12 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '3a2cfadaa480d20127069116dc575593a4e94523c95c232c1e03fe331e2d9cadc9e1a39b0703a18942ffcbc6e5a0b1cc3562d32f27558b0c2d1bcbe74720870e'
